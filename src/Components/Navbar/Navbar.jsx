d6b51a27aa077dff5d1ee5cfc2f499ecd86b4cd6
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-09-23 04:11:41 +0000

    structure

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+import logo from '../../assets/logo.png'
+
+function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="" />
+    </nav>
+  )
+}
+
+export default Navbar
